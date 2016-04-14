c965e7a1558a12c902002d6bda1f448e1cabacf8
apple <apple@woshi39.local>
2016-04-14 14:48:24 +0800

    chushihuaxmu

---
+++ 0414wangyixinwen1447/0414wangyixinwen1447/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0414wangyixinwen1447
+//
+//  Created by apple on 16/4/14.
+//  Copyright © 2016年 eason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0414wangyixinwen1447/0414wangyixinwen1447/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  0414wangyixinwen1447
+//
+//  Created by apple on 16/4/14.
+//  Copyright © 2016年 eason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    int a;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
